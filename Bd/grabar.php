a0f6f00f6dec04b9e9ed18041d6f2fa1154fe859
Kennet Castillo <elkennet46@gmail.com>
2018-08-31 23:09:29 -0600

    otro

--- Bd/grabar.php
+++ Bd/grabar.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexionBD.php';
-$stm=$conn->prepare("INSERT INTO alumnos (nombre1, nombre2) values (??)");
-$stm->bind_param('ss',$_POST['nombre1'],$_POST['nombre2']);
+$stm=$conn->prepare("INSERT INTO alumnos (nombre1, nombre2,apellido1,apellido2,correo,telefono,facultad) values (?,?,?,?,?,?,?)");
+$stm->bind_param('sssssss',$_POST['nombre1'],$_POST['nombre2'],$_POST['apellido1'],$_POST['apellido2'],$_POST['correo1'],$_POST['telefono1'],$_POST['namefacultad']);
 
 $stm->execute();
 if($stm->affected_rows>0)
@@ -12,7 +12,7 @@ if($stm->affected_rows>0)
             window.location.href="../index.php";
             </script>
         ';
-   //header('Location: ../index.php');
+    //header('Location: ../index.php');
 }else
 {
     echo '
@@ -24,4 +24,3 @@ if($stm->affected_rows>0)
 }
 $stm->close();
 $conn->close();
-
